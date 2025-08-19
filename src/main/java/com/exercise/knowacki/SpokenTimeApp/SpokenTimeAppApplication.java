1feba501063015302a1e3637828f958b388e1e72
Karol Nowacki <karolnowacki5@gmail.com>
2025-08-19 14:11:20 +0200

    Initial Spring boot project setup

---
+++ src/main/java/com/exercise/knowacki/SpokenTimeApp/SpokenTimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.knowacki.SpokenTimeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpokenTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpokenTimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/knowacki/SpokenTimeApp/SpokenTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.knowacki.SpokenTimeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpokenTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
