c800d4e0dd01531bfe1befd32a863acff49c65f1
Karol Nowacki <karolnowacki5@gmail.com>
2025-08-24 20:24:40 +0200

    Integration test configuration and parametrized tests added into SpokenTimeControllerTest

--- src/test/java/com/exercise/knowacki/SpokenTimeApp/controller/SpokenTimeControllerTest.java
+++ src/test/java/com/exercise/knowacki/SpokenTimeApp/controller/SpokenTimeControllerTest.java
@@ -3,61 +3,37 @@ package com.exercise.knowacki.SpokenTimeApp.controller;
 import com.exercise.knowacki.SpokenTimeApp.service.SpokenTimeService;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@WebMvcTest(SpokenTimeController.class)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 class SpokenTimeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockitoSpyBean
     private SpokenTimeService service;
 
+    @ParameterizedTest(name="Time: {0}, Spoken form: {1}")
+    @CsvFileSource(resources = "/spokenTimeServiceTestData.csv", numLinesToSkip = 1)
     @SneakyThrows
-    @Test
-    void shouldReturnSpokenTimeForValidInput() {
-        // Given
-        String timeInput = "14:00";
-        when(service.convertToSpokenForm(timeInput)).thenReturn("two o'clock");
+    void shouldReturnSpokenTimeForValidInput(String testInput, String expectedOutput) {
 
-        // When & Then
-        mockMvc.perform(get("/spoken-time/GB/{time}", timeInput))
+        mockMvc.perform(get("/spoken-time/GB/{time}", testInput))
                 .andExpect(status().isOk())
-                .andExpect(content().string("two o'clock"));
-    }
-
-    @SneakyThrows
-    @Test
-    void shouldReturnMidnightFor00_00(){
-        // Given
-        String timeInput = "00:00";
-        when(service.convertToSpokenForm(timeInput)).thenReturn("midnight");
-
-        // When & Then
-        mockMvc.perform(get("/spoken-time/GB/{time}", timeInput))
-                .andExpect(status().isOk())
-                .andExpect(content().string("midnight"));
-    }
-
-    @SneakyThrows
-    @Test
-    void shouldReturnNoonFor12_00() {
-        // Given
-        String timeInput = "12:00";
-        when(service.convertToSpokenForm(timeInput)).thenReturn("noon");
-
-        // When & Then
-        mockMvc.perform(get("/spoken-time/GB/{time}", timeInput))
-                .andExpect(status().isOk())
-                .andExpect(content().string("noon"));
+                .andExpect(content().string(expectedOutput));
     }
 
     @SneakyThrows
@@ -65,8 +41,6 @@ class SpokenTimeControllerTest {
     void shouldReturnBadRequestForInvalidTimeFormat() {
         // Given
         String timeInput = "25:00";
-        when(service.convertToSpokenForm(timeInput))
-                .thenThrow(new IllegalArgumentException("Invalid time format. Please use HH:mm (e.g., 14:30)"));
 
         // When & Then
         mockMvc.perform(get("/spoken-time/GB/{time}", timeInput))
@@ -74,7 +48,7 @@ class SpokenTimeControllerTest {
                 .andExpect(jsonPath("$.statusCode").value(400))
                 .andExpect(jsonPath("$.requestedHour").value("25:00"))
                 .andExpect(jsonPath("$.error").value("IllegalArgumentException"))
-                .andExpect(jsonPath("$.errorMessage").value("Invalid time format. Please use HH:mm (e.g., 14:30)"));
+                .andExpect(jsonPath("$.errorMessage").value("Invalid time format. Please use HH:mm (e.g., 14:30, 06:02)"));
     }
 
     @SneakyThrows
@@ -82,8 +56,6 @@ class SpokenTimeControllerTest {
     void shouldReturnBadRequestForNonNumericInput() {
         // Given
         String timeInput = "invalid";
-        when(service.convertToSpokenForm(timeInput))
-                .thenThrow(new IllegalArgumentException("Invalid time format. Please use HH:mm (e.g., 14:30)"));
 
         // When & Then
         mockMvc.perform(get("/spoken-time/GB/{time}", timeInput))
@@ -91,6 +63,6 @@ class SpokenTimeControllerTest {
                 .andExpect(jsonPath("$.statusCode").value(400))
                 .andExpect(jsonPath("$.requestedHour").value("invalid"))
                 .andExpect(jsonPath("$.error").value("IllegalArgumentException"))
-                .andExpect(jsonPath("$.errorMessage").value("Invalid time format. Please use HH:mm (e.g., 14:30)"));
+                .andExpect(jsonPath("$.errorMessage").value("Invalid time format. Please use HH:mm (e.g., 14:30, 06:02)"));
     }
 }
